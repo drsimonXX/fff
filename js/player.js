e1d7d63366154c8dc5fd51616b9d67380f8cb292
drsimonXX <drsimonXX@users.noreply.github.com>
2016-09-27 00:28:41 -0500

    Added new podcasts

--- js/player.js
+++ js/player.js
@@ -1,4 +1,6 @@
-var podcasts = ["colin-kaepernick.mp3",
+var podcasts = ["cfb-week-4.mp3",
+				"michigan-week-4.mp3",
+				"colin-kaepernick.mp3",
 				"coaches-hot-seat.mp3",
 				"week-3-cfb.mp3",
 				"michigan-week-3.mp3"];
@@ -10,10 +12,12 @@ var duration = document.getElementById('duration');
 var volumeSlider = document.getElementById('volumeSlider');
 var nextPodcastTitle = document.getElementById('nextPodcastTitle');
 
-var podcastOne = "Sept. 19, 2016<br>Clemson head coach Dabo Swinney recently added his name to the long list of people who have criticized athletes who are protesting the national anthem. Find out why he--and other coaches and players--would be better off keeping their mouths shut."; 
-var podcastTwo = "Sept. 19, 2016<br>Out of all the college football coaches who entered the season on the hot seat, which one will definitely be out at the end of the season?";
-var podcastThree = "Sept. 19, 2016<br>Two huge upsets happened in week 3 in college football with Florida State getting smoked by Louisville and Iowa getting shocked at home by FCS powerhouse North Dakota State. Which upset was the biggest?";
-var podcastFour = "Sept. 19, 2016<br>Week 3 Michigan football breakdown--reviewing the shocking first half against Colorado and discussing what this could mean for the rest of Michigan's season, as well as Michigan's Big Ten title and playoff hopes.";
+var podcastThree = "Sept. 19, 2016<br>Clemson head coach Dabo Swinney recently added his name to the long list of people who have criticized athletes who are protesting the national anthem. Find out why he--and other coaches and players--would be better off keeping their mouths shut."; 
+var podcastFour = "Sept. 19, 2016<br>Out of all the college football coaches who entered the season on the hot seat, which one will definitely be out at the end of the season?";
+var podcastFive = "Sept. 19, 2016<br>Two huge upsets happened in week 3 in college football with Florida State getting smoked by Louisville and Iowa getting shocked at home by FCS powerhouse North Dakota State. Which upset was the biggest?";
+var podcastSix = "Sept. 19, 2016<br>Week 3 Michigan football breakdown--reviewing the shocking first half against Colorado and discussing what this could mean for the rest of Michigan's season, as well as Michigan's Big Ten title and playoff hopes.";
+var podcastOne = "Sept. 27, 2016<br>It's starting early this season--Les Miles is out at LSU, and Brian Kelly's seat is warming up at Notre Dame. Plus, I'm calling out ESPN and College GameDay, Southern bias and B1G-time disrespect.";
+var podcastTwo = "Sept. 26, 2016<br>Week 4 saw Michigan take care of business against Penn State while Wisconsin shocked Michigan State. Can Wisconsin pull off another upset in the Big House this Saturday?";
 
 var podcast = new Audio();
 
@@ -80,8 +84,12 @@ function next(){
 		firstPodcast.innerHTML = podcastTwo;
 	} else if (currentPodcast === 2) {
 		firstPodcast.innerHTML = podcastThree;
-	} else {
+	} else if (currentPodcast === 3) {
 		firstPodcast.innerHTML = podcastFour;
+	} else if (currentPodcast === 4) {
+		firstPodcast.innerHTML = podcastFive;
+	} else {
+		firstPodcast.innerHTML = podcastSix;
 		nextPodcastTitle.innerHTML = "Next Podcast: " + podcasts[0];
 	}
 	
@@ -104,8 +112,12 @@ function previous () {
 		firstPodcast.innerHTML = podcastTwo;
 	} else if (currentPodcast === 2) {
 		firstPodcast.innerHTML = podcastThree;
-	} else {
+	} else if (currentPodcast === 3) {
 		firstPodcast.innerHTML = podcastFour;
+	} else if (currentPodcast === 4) {
+		firstPodcast.innerHTML = podcastFive;
+	} else {
+		firstPodcast.innerHTML = podcastSix;
 		nextPodcastTitle.innerHTML = "Next Podcast: " + podcasts[0];
 	}
 }
